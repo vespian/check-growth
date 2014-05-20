cf5576615e6b257d12e0e93cb5b8bd346543b9b8
Vespian <vespian@wp.pl>
2014-05-20 21:29:23 +0200

    Fix licensing info

--- check_growth/__init__.py
+++ check_growth/__init__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# Copyright (c) 2014 Pawel Rozlach
 # Copyright (c) 2014 Zadane.pl sp. z o.o.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not
